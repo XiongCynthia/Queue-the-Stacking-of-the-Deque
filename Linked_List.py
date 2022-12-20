6f5e4c1ca1b05dfa8be203c3794bbb5b64b3b873
Cynthia Xiong <cynthia.xiong@outlook.com>
2022-12-20 15:13:53 -0500

    Delete TODO comments

--- Linked_List.py
+++ Linked_List.py
@@ -3,16 +3,11 @@ class Linked_List:
     class __Node:
         
         def __init__(self, val):
-            # Declare and initialize the public attributes for objects of the
-            # Node class. TODO replace pass with your implementation
             self.val = val
             self.prev = None
             self.next = None
 
     def __init__(self):
-        # Declare and initialize the private attributes for objects of the
-        # sentineled Linked_List class TODO replace pass with your
-        # implementation
         self.__header = Linked_List.__Node(None)
         self.__trailer = Linked_List.__Node(None)
         self.__header.next = self.__trailer
@@ -20,14 +15,10 @@ class Linked_List:
         self.__size = 0
 
     def __len__(self):
-        # Return the number of value-containing nodes in this list. TODO replace
-        # pass with your implementation
         return self.__size
 
     def append_element(self, val):
-        # Increase the size of the list by one, and add a node containing val at
-        # the new tail position. this is the only way to add items at the tail
-        # position. TODO replace pass with your implementation
+        # This is the only way to add items at the tail position.
         # Instantiate a new node
         new_node = self.__Node(val)
         # Appending the new node
@@ -39,12 +30,7 @@ class Linked_List:
         self.__size = self.__size+1
 
     def insert_element_at(self, val, index):
-        # Assuming the head position (not the header node) is indexed 0, add a
-        # node containing val at the specified index. If the index is not a
-        # valid position within the list, raise an IndexError exception. This
-        # method cannot be used to add an item at the tail position. TODO
-        # replace pass with your implementation
-        # Check if index zis too large (or at the tail) or negative
+        # Check if index is too large (or at the tail) or negative
         if index > self.__size-1 or index < 0:
             raise IndexError
         # Instantiate a new node
@@ -69,10 +55,6 @@ class Linked_List:
         self.__size = self.__size+1
 
     def remove_element_at(self, index):
-        # Assuming the head position (not the header node) is indexed 0, remove
-        # and return the value stored in the node at the specified index. If the
-        # index is invalid, raise an IndexError exception. TODO replace pass
-        # with your implementation
         # Check if index is too large or negative
         if index >= self.__size or index < 0:
             raise IndexError
@@ -97,10 +79,6 @@ class Linked_List:
         return cur.val
 
     def get_element_at(self, index):
-        # Assuming the head position (not the header node) is indexed 0, return
-        # the value stored in the node at the specified index, but do not unlink
-        # it from the list. If the specified index is invalid, raise an
-        # IndexError exception. TODO replace pass with your implementation
         # Check if index is too large or negative
         if index >= self.__size or index < 0:
             raise IndexError
@@ -117,11 +95,7 @@ class Linked_List:
         return cur.val
 
     def rotate_left(self):
-        # Rotate the list left one position. Conceptual indices should all
-        # decrease by one, except for the head, which should become the tail.
-        # For example, if the list is [ 5, 7, 9, -4 ], this method should alter
-        # it to [ 7, 9, -4, 5 ]. This method should modify the list in place and
-        # must not return a value. TODO replace pass with your implementation.
+        # Rotate the list left one position (e.g., [ 5, 7, 9, -4 ] to [ 7, 9, -4, 5 ]).
         # Check if the list is empty
         if self.__size == 0:
             return
@@ -137,13 +111,6 @@ class Linked_List:
         self.__trailer.prev = node
         
     def __str__(self):
-        # Return a string representation of the list's contents. An empty list
-        # should appear as [ ]. A list with one element should appear as [ 5 ].
-        # A list with two elements should appear as [ 5, 7 ]. You may assume
-        # that the values stored inside of the node objects implement the
-        # __str__() method, so you call str(val_object) on them to get their
-        # string representations. TODO replace pass with your implementation
-        # Create a new list with the same size as the linked list
         # Check if the list is empty
         if self.__size == 0:
             return '[ ]'
@@ -163,16 +130,10 @@ class Linked_List:
         return ''.join(items)
 
     def __iter__(self):
-        # Initialize a new attribute for walking through your list TODO insert
-        # your initialization code before the return statement. Do not modify
-        # the return statement.
         self.__iter_cur = self.__header
         return self
 
     def __next__(self):
-        # Using the attribute that you initialized in __iter__(), fetch the next
-        # value and return it. If there are no more values to fetch, raise a
-        # StopIteration exception. TODO replace pass with your implementation
         if self.__iter_cur.next is self.__trailer:
             raise StopIteration
         else:
@@ -180,15 +141,6 @@ class Linked_List:
         return self.__iter_cur.val
 
     def __reversed__(self):
-        # Construct and return a new Linked_List object whose nodes alias the
-        # same objects as the nodes in this list, but in reverse order. For a
-        # Linked_List object ll, Python will automatically call this function
-        # when it encounters a call to reversed(ll) in an application. If
-        # print(ll) displays [ 1, 2, 3, 4, 5 ], then print(reversed(ll)) should
-        # display [ 5, 4, 3, 2, 1 ]. This method does not change the state of
-        # the object on which it is called. Calling print(ll) again will still
-        # display [ 1, 2, 3, 4, 5 ], even after calling reversed(ll). This
-        # method must operate in linear time.
         new_list = Linked_List()
         cur_original = self.__trailer.prev # To take values from the original list starting from the tail
         cur_new = new_list.__header # To add new nodes starting from the header
@@ -204,63 +156,4 @@ class Linked_List:
         return new_list
 
 if __name__ == '__main__':
-    # Your test code should go here. Be sure to look at cases when the list is
-    # empty, when it has one element, and when it has several elements. Do the
-    # indexed methods raise exceptions when given invalid indices? Do they
-    # position items correctly when given valid indices? Does the string
-    # representation of your list conform to the specified format? Does removing
-    # an element function correctly regardless of that element's location? Does
-    # a for loop iterate through your list from head to tail? Does a for loop
-    # iterate through your reversed list from tail to head? Your writeup should
-    # explain why you chose the test cases. Leave all test cases in your code
-    # when submitting. TODO replace pass with your tests
-    
-    # Linked lists
-    ll_5 = Linked_List() # 5 elements
-    ll_5.append_element(0)
-    ll_5.append_element(1)
-    ll_5.append_element(2)
-    ll_5.append_element(3)
-    ll_5.append_element(4)
-    ll_0 = Linked_List() # Empty
-    ll_1 = Linked_List() # 1 element
-    ll_1.append_element(0)
-
-    # Test methods
-    # ll_5.append_element('a')
-    # ll_0.append_element('a')
-    # ll_1.append_element('a')
-    # ll_5.insert_element_at('b',0)
-    # ll_1.insert_element_at('b',0)
-    # ll_5.insert_element_at('c',3)
-    # ll_0.insert_element_at('b',0) # Should raise error
-    # ll_5.remove_element_at(0)
-    # ll_1.remove_element_at(0)
-    # ll_5.remove_element_at(3)
-    # ll_0.remove_element_at(0) # Should raise error
-    # print(ll_5.get_element_at(3))
-    # print(ll_1.get_element_at(0))
-    # ll_0.get_element_at(0) # Should raise error
-    # for i in range(8):
-    #     ll_5.rotate_left()
-    # ll_1.rotate_left()
-    # ll_0.rotate_left()
-    # print(reversed(ll_5))
-    # print(reversed(ll_1))
-    # print(reversed(ll_0))
-    # for i in range(8):
-    #     print(i, reversed(ll_5))
-    # for val in ll_5:
-    #     print(val)
-    # for val in ll_0:
-    #     print(val)
-    # for val in reversed(ll_5):
-    #     print(val)
-    
-    # Show contents and size
-    print(str(ll_5))
-    print('size:', len(ll_5))
-    print(str(ll_0))
-    print('size:', len(ll_0))
-    print(str(ll_1))
-    print('size:', len(ll_1))
+    pass
