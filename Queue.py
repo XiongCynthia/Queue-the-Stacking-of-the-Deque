15ec7335d1d112b94d4925be3ba495304477cde5
Cynthia Xiong <cynthia.xiong@outlook.com>
2022-12-20 15:15:47 -0500

    Delete TODO comments

--- Queue.py
+++ Queue.py
@@ -6,26 +6,20 @@ class Queue:
     self.__dq = get_deque()
 
   def __str__(self):
-    # TODO replace pass with your implementation.
-    # Orient your string from front (left) to back (right).
     return str(self.__dq)
 
   def __len__(self):
-    # TODO replace pass with your implementation.
     return len(self.__dq)
 
   def enqueue(self, val):
-    # TODO replace pass with your implementation.
     self.__dq.push_back(val)
 
   def dequeue(self):
-    # TODO replace pass with your implementation.
     if len(self.__dq) == 0:
       return None
     return self.__dq.pop_front()
 
   def peek(self):
-    # TODO replace pass with your implementation.
     if len(self.__dq) == 0:
       return None
     return self.__dq.peek_front()
